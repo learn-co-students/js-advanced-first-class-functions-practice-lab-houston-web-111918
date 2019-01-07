f5ec2f5791db735716e79c197cef0e2e7a3d07e7
rdaley321 <rdaley321@yahoo.com>
2019-01-07 15:26:56 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function logDriverNames(arr){
+  for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i].name)
+  }
+}
+
+function logDriversByHometown(arr,loc){
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i].hometown === loc){
+      console.log(arr[i].name)
+    }
+  }
+}
+
+function driversByRevenue(arr){
+  return arr.map(x => x).sort((a,b) => a.revenue - b.revenue)
+}
+
+function driversByName(arr) {
+  return arr.slice().sort((a,b) => a.name.localeCompare(b.name))
+}
+
+function totalRevenue(arr){
+  return arr.map(x => x.revenue).reduce((a,cv) => a + cv)
+}
+
+function averageRevenue(arr){
+  return arr.map(x => x.revenue).reduce((a,cv) => a + cv)/arr.length
+}
