02ff609bb27d3adf25be71a2debefc996022e75b
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2019-01-07 15:39:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  for (let i = 0; i < drivers.length; i++) {
+    console.log(drivers[i].name)
+  }
+}
+
+function logDriversByHometown(drivers, ht) {
+  for (let i = 0; i < drivers.length; i++) {
+    if (drivers[i].hometown === ht) {
+      console.log(drivers[i].name);
+    }
+  }
+}
+
+function driversByRevenue(drivers) {
+  return drivers.map(x => x).sort((a, b) => a.revenue - b.revenue);
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort((d1, d2) => d1.name.localeCompare(d2.name));
+}
+
+function totalRevenue(drivers) {
+  let total = 0;
+  for (let i = 0; i < drivers.length; i++) {
+    total += drivers[i].revenue;
+  }
+  return total;
+}
+
+function averageRevenue(drivers) {
+  let total = 0;
+  for (let i = 0; i < drivers.length; i++) {
+    total += drivers[i].revenue;
+  }
+  return total / drivers.length;
+}
